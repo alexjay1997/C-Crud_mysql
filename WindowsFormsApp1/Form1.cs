2f78c1654a2527f875af321876243c572741cc5c
alexjay1997 <aljayfauni@gmail.com>
2020-02-18 01:58:34 +0800

    initial commit

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password= ;database=loginform");
+        MySqlCommand command;
+        MySqlDataReader mdr;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Form1(MySqlConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           // if (textBox1.Text == "admin" && textBox2.Text =="123")
+        //    {
+                //show form2  ----- start ---- 
+              //  var form2 = new Form2();
+             //   form2.Show();
+                // end  ----------- end -------
+                // }
+
+
+            // LOGIN BUTTON
+
+            connection.Open();
+            string selectQuery = "SELECT * FROM userinfo WHERE Username = '" + textBox1.Text + "' AND Password = '" + textBox2.Text + "';";
+            command = new MySqlCommand(selectQuery, connection);
+            mdr = command.ExecuteReader();
+            if (mdr.Read())
+            {
+
+                var form2 = new Form2();
+                   form2.Show();
+
+            }
+            else
+            {
+
+                MessageBox.Show("Incorrect Login Information! Try again.");
+            }
+
+            connection.Close();
+
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Please input Username and Password", "Error");
+            }
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // ------   compute 2 numbers ------//csharp
+
+            int num1, num2, sum;
+            num1 = Convert.ToInt32(textBox3.Text);
+            num2 = Convert.ToInt32(textBox4.Text);
+            textBox5.Text = Convert.ToString((Convert.ToInt32(textBox3.Text) + Convert.ToInt32(textBox4.Text)));
+            sum = num1 + num2;
+           
+            MessageBox.Show(sum.ToString());
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        
+        MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password= ;database=loginform");
+        MySqlCommand command;
+        MySqlDataReader mdr;
+        MySqlDataAdapter da;
+
+        public Form2()
+        {
+            InitializeComponent();
+            DisplayData();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // *****************   insert function     **************************
+
+            connection.Open();
+
+            // ***************     checked if already exist  *********************
+
+            string check_query = "Select * from userinfo where username='"+textBox_username.Text+"'";
+            command = new MySqlCommand(check_query, connection);
+            mdr = command.ExecuteReader();
+            if (mdr.Read())
+            {
+
+                MessageBox.Show("already Exist!!");
+
+                connection.Close();
+            }
+      
+
+            //********************  End of Check    ************************
+
+
+            else
+            {
+                connection.Close();
+
+                connection.Open();
+                string Insert_query = "Insert into userinfo (username,password) values ('" + textBox_username.Text + "','" + textBox_password.Text + "')";
+                command = new MySqlCommand(Insert_query, connection);
+                mdr = command.ExecuteReader();
+                MessageBox.Show("successfully Added New Record!");
+
+
+                connection.Close();
+            }
+      
+        }
+
+        private void DisplayData() {
+
+            try
+            {
+                connection.Open();
+
+         
+                string SelectAll_query = "select * from userinfo ";
+                command = new MySqlCommand(SelectAll_query, connection);
+
+                da = new MySqlDataAdapter();
+                da.SelectCommand = command;
+                DataTable data_table = new DataTable();
+                da.Fill(data_table);
+                dataGridView1.DataSource = data_table; // here i have assign dTable object to the dataGridView1 object to display data.               
+                                                   // MyConn2.Close();  
+            }
+            catch (Exception ex)
+            {
+               // MessageBox.Show(ex.Message);
+            }
+            connection.Close();
+
+        }
+       
+    }
+}
